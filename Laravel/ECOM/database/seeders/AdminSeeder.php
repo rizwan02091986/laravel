65b72b92d267af802f8af1fbb4f91d1daf3b4b41
M.Rizwan <rizwan02091986@gmail.com>
2024-07-11 20:16:52 +0500

    first

---
+++ Laravel/ECOM/database/seeders/AdminSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Admin;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Admin::create([
+            'name'=> 'Ali',
+            'age'=> 20,
+            'gender'=> 'Male',
+            'designation'=> 'Admin',           
+        ]);
+    }
+}
